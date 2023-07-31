0749b3f5c5dcdeb08384a7652be9c6b3ab470be3
actyvystom <thomas.goerldt@actyvyst.com>
2023-07-31 12:49:15 +0200

    add local state to swr fetch

--- components/Joke/index.js
+++ components/Joke/index.js
@@ -3,9 +3,30 @@ import useSWR from "swr";
 
 export default function Joke() {
   const [id, setId] = useState(0);
+  const [jokesInfo, setJokesInfo] = useState([]);
   const { data, isLoading, error } = useSWR(
     `https://example-apis.vercel.app/api/bad-jokes/${id}`
   );
+
+  function handleToggleFunny(id) {
+    setJokesInfo((jokesInfo) => {
+      const info = jokesInfo.find((info) => info.id === id);
+      if (info) {
+        return jokesInfo.map((info) =>
+          info.id === id ? { ...info, isFunny: !info.isFunny } : info
+        );
+      }
+      return [...jokesInfo, { id, isFunny: true, copyright: "" }];
+    });
+  }
+
+  const info = jokesInfo.find(
+    (info) =>
+      info.id === id ?? {
+        isFunny: false,
+      }
+  );
+  const { isFunny } = jokesInfo;
   function handlePrevJoke() {
     setId(data.prevId);
   }
@@ -24,13 +45,24 @@ export default function Joke() {
   return (
     <>
       <small>ID: {id}</small>
-      <h1>{data.joke}</h1>
+      <h1>
+        {data.joke}
+        <span
+          role="img"
+          aria-label={info.isFunny ? "A laughing face" : "An unamused face"}
+        >
+          {info.isFunny ? "🤣" : "😒"}
+        </span>
+      </h1>
       <div>
         <button type="button" onClick={handlePrevJoke}>
           ← Prev Joke
         </button>
         <button type="button" onClick={handleNextJoke}>
           Next Joke →
+        </button>
+        <button type="button" onClick={() => handleToggleFunny(id)}>
+          {isFunny ? "Stop laughing" : "start laughing"}
         </button>
       </div>
     </>
